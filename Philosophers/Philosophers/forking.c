50a09d483005d8c635f9568bc291e492d3d5b7a2
Stronk_ <94312066+stronk-exe@users.noreply.github.com>
2022-03-24 20:20:22 +0100

    new commit

---
+++ Philosophers/Philosophers/forking.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   forking.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-asri <ael-asri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/21 21:55:51 by ael-asri          #+#    #+#             */
+/*   Updated: 2022/03/23 23:51:13 by ael-asri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	init_forkes(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	data->forks = malloc(sizeof(pthread_mutex_t) * data->n_philo);
+	if (!data->forks)
+		return (0);
+//	printf("hi\n");
+	while (i < data->n_philo)
+	{
+		pthread_mutex_init(&data->forks[i], NULL);
+		i++;
+	}
+	return (1);
+}
---
+++ Philosophers/Philosophers/threading.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   threading.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-asri <ael-asri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 22:59:52 by ael-asri          #+#    #+#             */
+/*   Updated: 2022/03/24 18:36:41 by ael-asri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+int	create_threads(t_data *data)
+{
+	int	i;
+
+	i = 0;
+	data->t = malloc(sizeof(t_philo) * data->n_philo);
+	if(!data->t)
+		return (0);
+//	if (pthread_mutex_init(data->philo->t, NULL) != 0)
+//		return (0);
+//	printf("t sleep %d\n", data->t_sleep);
+	while (i < data->n_philo)
+	{
+		
+		if (pthread_create(&data->t[i], NULL, &routine, (void *)&data->philo[i]) != 0)
+			return (0);
+		data->philo[i].id = i+1;
+		printf("I'm philo %d\n", data->philo[i].id);
+		i++;
+	}
+	i = 0;
+	while (i < data->n_philo)
+	{
+		if (pthread_join(data->t[i], NULL) != 0)
+			return (0);
+		i++;
+	}
+	return (1);
+}
