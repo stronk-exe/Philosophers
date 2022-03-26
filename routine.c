7190ac0e4c021a701f45b373d4ddf4f8d0e7c975
Stronk_ <94312066+stronk-exe@users.noreply.github.com>
2022-03-26 12:24:11 +0100

    Add files via upload

---
+++ routine.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   routine.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-asri <ael-asri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 23:13:37 by ael-asri          #+#    #+#             */
+/*   Updated: 2022/03/25 18:56:01 by ael-asri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philosophers.h"
+
+void	*routine(void *p)
+{
+//	t_philo *philo;
+	t_data	*data;
+	int	i;
+
+//	i = 0;
+	data = p;
+//	printf("n philos %d\n", data->n_philo);
+//	data = p;
+//	take_forks();
+//	pthread_mutex_lock(&(philo->lock));
+//	for(int j=0;j<data->n_philo;j++)
+//		printf("i'm philo %d\n", data->philo[j].id);
+//	while ((cc * 1000) < (time + philo->t_die) /*&& i < philo->meal*/)
+	while (1)
+	{
+	//	printf("raw time %ld\n", current_time.tv_sec);
+	//	printf("new time %ld\n", current_time.tv_sec + philo->t_die);
+	//	i = 1;
+	//	printf("cc %ld\n", cc);
+		i=0;
+		while (i < data->n_philo)
+		{
+		//	if (philo->n[i] )
+			eating(data, i);
+			sleeping(data, i);
+			thinking(&data->philo[i]);
+			i++;
+		}
+		
+	//	sleeping(philo);
+	//	thinking(philo, philo->n[i]);
+	//	i++;
+	}
+//	died(&data);
+//	pthread_mutex_destroy(philo->fork);
+	return(0);
+}
